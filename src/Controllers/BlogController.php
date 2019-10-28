6d0c20c013fd63c1963756ed608ef00994cc68cb
Morgane <mrebindaine@hotmail.com>
2019-10-28 10:10:54 +0100

    Create homepage

--- src/Controllers/BlogController.php
+++ src/Controllers/BlogController.php
@@ -2,10 +2,24 @@
 
 namespace OCR5\Controllers;
 
+use Twig\TwigFunction;
+
 class BlogController
 {
     public function home()
     {
-        echo 'this is the home from the controller';
+//        require ('../templates/test.html');die();
+        $loader = new \Twig\Loader\FilesystemLoader('../templates');
+        $twig = new \Twig\Environment($loader, [
+//            'cache' => '../cache'
+        ]);
+        $twig->addFunction(new TwigFunction('css', function($value) {
+            return  'style/' .$value. '.css';
+        }));
+        $twig->addFunction(new TwigFunction('img', function($value) {
+            return  'img/' .$value;
+        }));
+
+        echo $twig->render('blog/home.html.twig');
     }
 }
